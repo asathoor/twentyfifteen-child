895c3d2afe927077dad17f8188099d9a64efedd2
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2016-04-03 15:26:25 +0200

    Removed weird xxxses

--- index.php
+++ index.php
@@ -21,8 +21,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<h1>XXXXXXXXXXXXXXXXXXXXXXX</h1>
-
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
